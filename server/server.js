99e4f8929c2ffe8aa46406983842caae1e5622e8
Aden Navi <aden7x@gmail.com>
2019-01-03 19:25:51 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ let bodyParser = require('body-parser');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 let app = express();
 
@@ -27,6 +28,24 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+
+      res.status(200).send({todo});
+    }, (e) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
